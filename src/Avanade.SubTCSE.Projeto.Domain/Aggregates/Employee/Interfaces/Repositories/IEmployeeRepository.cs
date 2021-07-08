71e847ddb853bc76a868a0fea646a46f54143989
Juliane-C <juliane.cristinara97@gmail.com>
2021-07-08 14:12:10 -0300

    correcao do classe BaseRepository e criacao dos repositorios EmployeeRepository e EmployeeRoleRepository

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Interfaces/Repositories/IEmployeeRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
 
-namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interfaces.Repository
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interfaces.Repositories
 {
     public interface IEmployeeRepository : IBaseRepository<Entities.Employee, string>
     {
